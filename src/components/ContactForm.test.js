d5931ad994439c68ee49754f20fd843e34ff4c4d
Gordon Caister <gordoncaister@gmail.com>
2020-04-09 19:55:29 -0500

    finished mvp

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -27,3 +27,38 @@ test('inputs are correct type', () => {
         expect(email).toHaveAttribute("type","email")
 
     });
+
+test('inputs are correct type', () => {
+
+    const { getByLabelText } = render(<ContactForm />);
+    
+        const email = getByLabelText(/email/i);
+        
+        
+        expect(email).toHaveAttribute("type","email")
+
+    });
+
+test("make sure form is submitting", ()=> {
+
+    const { getByLabelText, getByText } = render(<ContactForm />);
+     
+    const firstName = getByLabelText(/first name/i); 
+    const lastName = getByLabelText(/last Name/i);
+    const email = getByLabelText(/email/i);
+    const message = getByLabelText(/message/i);
+
+    fireEvent.change(firstName, {target: {value: "gordon"}});
+    fireEvent.change(lastName, {target: {value: "caister"}});
+    fireEvent.change(email, {target: {value: "gordon@gmail.com"}});
+    fireEvent.change(message, {target: {value: "gordon is a clown"}});
+
+    expect(firstName.value).toBe("gordon");
+    expect(lastName.value).toBe("caister");
+    expect(email.value).toBe("gordon@gmail.com");
+    expect(message.value).toBe("gordon is a clown");
+
+
+
+
+});
